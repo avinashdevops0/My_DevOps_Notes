f1356eccc85fefc43d4771a6a1c85084d8ec63ef
avinash-ganta0 <avinash.ganta6@gmail.com>
2026-01-29 19:49:31 +0530

    code

--- microservices-app/api-gateway/server.js
+++ microservices-app/api-gateway/server.js
@@ -4,20 +4,20 @@ const httpProxy = require('http-proxy');
 const app = express();
 const port = 3000;
 
-const proxy = httpProxy.createProxyServer();
+const proxy = httpProxy.createProxyServer({ changeOrigin: true });
 
-// Proxy routes
-app.use('/users', (req, res) => {
-  proxy.web(req, res, { target: 'http://users-service:3001' });
-});
+function proxyService(basePath, target) {
+  app.all(basePath, (req, res) => {
+    proxy.web(req, res, { target });
+  });
+  app.all(`${basePath}/*`, (req, res) => {
+    proxy.web(req, res, { target });
+  });
+}
 
-app.use('/products', (req, res) => {
-  proxy.web(req, res, { target: 'http://products-service:3002' });
-});
-
-app.use('/orders', (req, res) => {
-  proxy.web(req, res, { target: 'http://orders-service:3003' });
-});
+proxyService('/users', 'http://users-service:3001');
+proxyService('/products', 'http://products-service:3002');
+proxyService('/orders', 'http://orders-service:3003');
 
 app.listen(port, () => {
   console.log(`API Gateway listening at http://localhost:${port}`);
--- microservices-app/product-service/server.js
+++ microservices-app/product-service/server.js
@@ -6,7 +6,6 @@ const port = 3002;
 
 app.use(express.json());
 
-// MySQL connection
 const db = mysql.createConnection({
   host: 'mysql',
   user: 'root',
@@ -14,55 +13,50 @@ const db = mysql.createConnection({
   database: 'microservices_db'
 });
 
-db.connect((err) => {
-  if (err) {
-    console.error('Error connecting to MySQL:', err);
-    return;
-  }
-  console.log('Connected to MySQL');
-});
+const connectWithRetry = () => {
+  db.connect(err => {
+    if (err) {
+      console.error('MySQL not ready, retrying in 5s...', err.message);
+      setTimeout(connectWithRetry, 5000);
+    } else {
+      console.log('Connected to MySQL');
+    }
+  });
+};
+
+connectWithRetry();
 
 // Routes
 app.get('/products', (req, res) => {
   db.query('SELECT * FROM products', (err, results) => {
-    if (err) throw err;
+    if (err) return res.status(500).json(err);
     res.json(results);
   });
 });
 
 app.get('/products/:id', (req, res) => {
-  const { id } = req.params;
-  db.query('SELECT * FROM products WHERE id = ?', [id], (err, results) => {
-    if (err) throw err;
-    res.json(results[0]);
-  });
+  db.query(
+    'SELECT * FROM products WHERE id = ?',
+    [req.params.id],
+    (err, results) => {
+      if (err) return res.status(500).json(err);
+      res.json(results[0]);
+    }
+  );
 });
 
 app.post('/products', (req, res) => {
   const { name, price } = req.body;
-  db.query('INSERT INTO products (name, price) VALUES (?, ?)', [name, price], (err, results) => {
-    if (err) throw err;
-    res.json({ id: results.insertId, name, price });
-  });
+  db.query(
+    'INSERT INTO products (name, price) VALUES (?, ?)',
+    [name, price],
+    (err, results) => {
+      if (err) return res.status(500).json(err);
+      res.json({ id: results.insertId, name, price });
+    }
+  );
 });
 
-app.put('/products/:id', (req, res) => {
-  const { id } = req.params;
-  const { name, price } = req.body;
-  db.query('UPDATE products SET name = ?, price = ? WHERE id = ?', [name, price, id], (err, results) => {
-    if (err) throw err;
-    res.json({ id, name, price });
-  });
-});
-
-app.delete('/products/:id', (req, res) => {
-  const { id } = req.params;
-  db.query('DELETE FROM products WHERE id = ?', [id], (err, results) => {
-    if (err) throw err;
-    res.json({ message: 'Product deleted' });
-  });
-});
-
-app.listen(port, () => {
-  console.log(`Product service listening at http://localhost:${port}`);
+app.listen(port, '0.0.0.0', () => {
+  console.log(`Product service listening on port ${port}`);
 });
